418dc93329c54bf52b485585b449058400677b62
Parmar Parth <55745745+Parth123-Pro@users.noreply.github.com>
2022-08-12 21:51:55 +0530

    Update Messenger.sol

--- Messenger/Messenger.sol
+++ Messenger/Messenger.sol
@@ -1,27 +1,23 @@
-pragma solidity >=0.7.0 <0.9.0;
-
-contract Messenger {
-
+pragma solidity ^0.5.1;
+ 
+contract Messenger{
     address owner;
     string[] messages;
-
-    constructor() public{
+    
+    constructor() public {
         owner = msg.sender;
-
     }
-
-    function add(string newMessage) public {
+    
+    function add(string memory newMessage) public {
         require(msg.sender == owner);
         messages.push(newMessage);
-    } 
-
-    function count() view public returns (uint) {
+    }
+    
+    function count() view public returns(uint){
         return messages.length;
     }
-
-    function getMessages(uint index) view public returns (string) {
+    
+    function getMessages(uint index) view public returns(string memory){
         return messages[index];
     }
-
-
 }
